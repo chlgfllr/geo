2a7e5a9dc1336eaf27467eb8bb82eb0ff617fe17
Chloe Gfeller <chloe.gfeller@gmail.com>
2018-02-10 17:13:04 +0100

    Add files for fixed length window

--- distances/okay/abxpy_to_easyabx.R
+++ distances/okay/abxpy_to_easyabx.R
@@ -1,14 +1,20 @@
 acoustic <- readr::read_delim("/Users/chloe/geo/acoustic_measures.txt", "\t")
 
-# We want to use fixed length windows because ABXpy normalizes times but Easy ABX doesn't if I understand well?
+# We want to use fixed length windows because ABXpy normalizes times but Easy ABX doesn't I think?
 ## Get minimum constriction time to set the left edge of the window
+## Note: I substract interburst duration because constriction time is taken up to
+## glottal release for ejectives and up to oral release for aspirated stops.
+## As interburst=0 for aspirated stops, it won't change the aspirates' constriction
 acoustic$constriction_norm <- as.numeric(acoustic$constriction - acoustic$interburst)
+
 ### Take min value because I don't want other material to interfere
 min_constriction <- min(acoustic$constriction_norm)
+
 ## Get average vowel duration to set the right edge of the window
+## Note: I add interburst duration for the same reason as I substracted it for
+## the constriction time. 
 acoustic$vowel_oral_burst <- acoustic$`v-duration` + acoustic$interburst
 mean_vowel <- mean(acoustic$vowel_oral_burst)
-qu3_vowel <- as.numeric(quantile(acoustic$vowel_oral_burst, .75))
 
 data_item_ <- readr::read_delim("data_okay.item", " ")
 
@@ -19,3 +25,6 @@ mean_dur <- mean(data_item_$duration)
 ## We had a mean CV window duration of mean_dur
 ## We will now have a shorter window of min_constriction + mean_vowel
 fixed_length <- qu1_constriction + mean_vowel
+
+## ABXpy run on fixed length window
+data <- readr::read_delim("/Users/chloe/geo/fixed_length/data_fixed.csv", "\t")
