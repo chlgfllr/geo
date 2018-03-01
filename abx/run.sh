759789722d95712853dad7ac8d2aa4c1b900473f
Chloe Gfeller <chloe.gfeller@gmail.com>
2018-03-01 14:15:21 +0100

    Move run.sh

--- abx/run.sh
+++ abx/run.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-FEATURE_FILE=../mono_mfcc.h5f
-ITEM_FILE=abx.item
+DIR=fixed
 
-TASK_FILE=abx.task
-DISTANCE_FILE=abx.distance
-SCORE_FILE=abx.score
+FEATURE_FILE=mono_mfcc.h5f
+ITEM_FILE=$DIR/abx.item
 
-RESULTS_FILE=results.csv
-OUTPUT_TXT=distance.txt
+TASK_FILE=$DIR/abx.task
+DISTANCE_FILE=$DIR/abx.distance
+SCORE_FILE=$DIR/abx.score
+
+RESULTS_FILE=$DIR/results.csv
+OUTPUT_TXT=$DIR/distance.txt
 
 
 abx-task $ITEM_FILE $TASK_FILE --on pulm --by place speaker question position vowel && \
@@ -16,6 +18,6 @@ abx-task $ITEM_FILE $TASK_FILE --on pulm --by place speaker question position vo
   abx-score $TASK_FILE $DISTANCE_FILE $SCORE_FILE && \
   abx-analyze $SCORE_FILE $TASK_FILE $RESULTS_FILE && \
   rm -f $OUTPUT_TXT && \
-  python ../dis2txt.py $TASK_FILE $DISTANCE_FILE $OUTPUT_TXT
+  python dis2txt.py $TASK_FILE $DISTANCE_FILE $OUTPUT_TXT
 
 
