262192634c9262b7ec4cf27570e3794eaa16dbc0
Chloe Gfeller <chloe.gfeller@gmail.com>
2018-03-01 14:02:57 +0100

    Add files

--- abx/fixed/run.sh
+++ abx/fixed/run.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 
-FEATURE_FILE=analysis_inputs/features__mfcc.h5f
-ITEM_FILE=analysis_inputs/item___for_h5f.item
+FEATURE_FILE=../mono_mfcc.h5f
+ITEM_FILE=abx.item
 
-TASK_FILE=$DIR/$BASENAME.task
-DISTANCE_FILE=$DIR/$BASENAME.distance
-SCORE_FILE=$DIR/$BASENAME.score
+TASK_FILE=abx.task
+DISTANCE_FILE=abx.distance
+SCORE_FILE=abx.score
 
-RESULTS_FILE=$DIR/$BASENAME.csv
+RESULTS_FILE=results.csv
+OUTPUT_TXT=distance.txt
 
 
+abx-task $ITEM_FILE $TASK_FILE --on pulm --by place speaker question position vowel && \
+  abx-distance $FEATURE_FILE $TASK_FILE $DISTANCE_FILE --normalization 1 --njobs 1 && \
+  abx-score $TASK_FILE $DISTANCE_FILE $SCORE_FILE && \
+  abx-analyze $SCORE_FILE $TASK_FILE $RESULTS_FILE && \
+  rm -f $OUTPUT_TXT && \
+  python ../dis2txt.py $TASK_FILE $DISTANCE_FILE $OUTPUT_TXT
 
-abx-task $ITEM_FILE $TASK_FILE --on type --by place speaker question position-wd following-v
-abx-distance $FEATURE_FILE $TASK_FILE $DISTANCE_FILE --normalization 1 --njobs 1
-abx-score $TASK_FILE $DISTANCE_FILE $SCORE_FILE
-abx-analyze $SCORE_FILE $TASK_FILE $RESULTS_FILE
+
